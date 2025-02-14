5646738e934a630da05b56532580294210548701
autran1010 <aubaotran123@gmail.com>
2025-02-14 11:45:11 +0700

    Fixbugs

--- server/server.js
+++ server/server.js
@@ -1,22 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 5000;
 
-app.use(cors()); // Allow frontend to call backend
-app.use(express.json()); // Parse JSON requests
-
-// Simple API route
-app.get("/hello", (req, res) => {
-    res.send("Hello from Express!");
-});
-app.post('/capitalize', (req, res) => {
-    const inputString = req.body.string;
-    if (typeof inputString === 'string') {
-        const capitalizedString = inputString.toUpperCase();
-        res.json({ capitalized: capitalizedString });
-    } else {
-        res.status(400).json({ error: 'Invalid input. Expected a string.' });
-    }
-});
+app.get("/api",(req, res) => {
+    res.json({"user": ["userOne", "userTwo", "userThree"]})
+})
 
 app.listen(5000, () => {console.log("server started on port 5000")})
